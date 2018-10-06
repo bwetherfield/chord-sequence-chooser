7d41032f523d866425e1b409df08000a5c415646
Ben Wetherfield <bwetherfield@gmail.com>
2018-10-06 15:51:28 -0400

    Add music21 chord to implementation

--- src/chordchoice.py
+++ src/chordchoice.py
@@ -4,10 +4,11 @@ from music21 import *
 
 class ChordNode:
 
-    def __init__(self, scale_degree, string_representation):
+    def __init__(self, scale_degree, string_representation, music21_chord):
         self.scale_degree = scale_degree
         self.string_representation = string_representation
         self.next_chords = []
+        self.internal_chord = music21_chord
 
     def add_follow_option(self, chord):
         self.next_chords.append(chord)
@@ -17,8 +18,8 @@ class ChordNode:
 
 if __name__ == "__main__":
 
-    chord_I = ChordNode(1, "I")
-    chord_iii = ChordNode(3, "iii")
+    chord_I = ChordNode(1, "I", chord.Chord(['C', 'E', 'G'])
+    chord_iii = ChordNode(3, "iii", chord.Chord(['E', 'G', 'B'])
     chord_I.add_follow_option(chord_iii)
 
     print("Choose your own harmonic adventure...")
