925a558f6148be0b1a9dc55d901829d7ffe495df
Ben Wetherfield <bwetherfield@gmail.com>
2018-10-06 15:31:13 -0400

    Add Chord class

--- src/chordchoice.py
+++ src/chordchoice.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+class Chord:
+
+    __init__(self, scale_degree, string_representation):
+        self.scale_degree = scale_degree
+        self.string_representation = string_representation
+
 if __name__ == "__main__":
 
     print("Choose your own harmonic adventure...")
