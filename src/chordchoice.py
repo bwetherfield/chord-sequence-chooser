ad0d4b3bc05100e627ce462708378fff53d944c7
Ben Wetherfield <bwetherfield@gmail.com>
2018-10-07 15:14:44 -0400

    Use linebreak divider

--- src/chordchoice.py
+++ src/chordchoice.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
 
     print("Choose your own harmonic adventure...")
     print(network_diagram)
-    print("Global commands: ", " ".join("({}) {}".format(str(i), str(x))
+    print("Global commands: ", "\n".join("({}) {}".format(str(i), str(x))
                                       for i,x in enumerate(global_functions)))
 
     while True:
